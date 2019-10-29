a7883800050d163bd0ed7c2373140fcc0b1f2172
Crystal Broach <crysland@gmail.com>
2019-10-28 21:39:00 -0500

    smashed farmers and cows

---
+++ src/javascripts/helpers/data/smash.js
@@ -0,0 +1,30 @@
+import cowData from './cows';
+import farmerData from './farmerData';
+import farmerCowData from './farmerCowData';
+
+// This file is where you write functions that bring
+// multiple collections together
+
+const getCompleteCows = () => new Promise((resolve, reject) => {
+  cowData.getCows().then((cows) => {
+    farmerData.getFarmers().then((farmers) => {
+      farmerCowData.getFarmerCows().then((farmerCows) => {
+        // do stuff
+        const finalCows = [];
+        cows.forEach((cow) => {
+          // spread operator below
+          const newCow = { ...cow };
+          // use the farmer cow record to get the
+          const farmerCowRecord = farmerCows.find((x) => x.cowId === cow.id);
+          const owner = farmers.find((x) => x.id === farmerCowRecord.farmerId);
+          newCow.owner = owner;
+          finalCows.push(newCow);
+        });
+        resolve(finalCows);
+      });
+    });
+  })
+    .catch((error) => reject(error));
+});
+
+export default { getCompleteCows };
