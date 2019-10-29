668d53a72c919fc65ba63f960233958723cb870f
Crystal Broach <crysland@gmail.com>
2019-10-28 19:14:25 -0500

    created nested promise to fetch data

--- src/javascripts/components/cowList/cowList.js
+++ src/javascripts/components/cowList/cowList.js
@@ -5,13 +5,8 @@ import './cowList.scss';
 const buildCows = () => {
   cowData.getCows()
     .then((response) => {
-      const demCows = response.data.cows;
-      const cows = [];
-      Object.keys(demCows).forEach((cowId) => {
-        demCows[cowId].id = cowId;
-        cows.push(demCows[cowId]);
-      });
-      console.log('it worked!', cows);
+      console.log('cow array from cowList', response);
+      // call a domstringBuilder function and pass in response (or cows)
     })
     .catch((error) => {
       console.error('shit broke', error);
